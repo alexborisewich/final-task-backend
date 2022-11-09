5c68da2fc964b8b1d7de4b81ab046d6333018a8a
Aliaksei Barysewich <alexboris61529@gmail.com>
2022-11-09 16:35:40 +0300

    feat: add settings for deploy

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://alexborisewich:alexboris61529a1b2c3d4@cluster0.jpnddsk.mongodb.net/managerApp');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
